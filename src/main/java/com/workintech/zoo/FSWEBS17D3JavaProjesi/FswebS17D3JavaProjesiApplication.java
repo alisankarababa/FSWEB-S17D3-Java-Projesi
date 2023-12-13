c1aa7c2ccc71ca4849bcc8595da5359fde8891a7
Alişan <alisankarababa@gmail.com>
2023-12-13 12:53:39 +0300

    init spring boot with dependencies dependencies

---
+++ src/main/java/com/workintech/zoo/FSWEBS17D3JavaProjesi/FswebS17D3JavaProjesiApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.zoo.FSWEBS17D3JavaProjesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FswebS17D3JavaProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FswebS17D3JavaProjesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/zoo/FSWEBS17D3JavaProjesi/FswebS17D3JavaProjesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.zoo.FSWEBS17D3JavaProjesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FswebS17D3JavaProjesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
